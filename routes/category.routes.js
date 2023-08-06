a6d0de4044e6b56f6622aeae87c707d6640d014e
Santiago Estevez <elbolaestevez@gmail.com>
2023-08-05 21:03:36 -0300

    removing category

--- routes/category.routes.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require("express");
-const CategoryController = require("../controllers/category.controller");
-
-const cateogoryRouter = Router();
-
-cateogoryRouter.post("/create", CategoryController.createCategory);
-cateogoryRouter.get("/", CategoryController.findCategories);
-
-module.exports = cateogoryRouter;
--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,12 @@ const { Router } = require("express");
 
 const userRoutes = require("./user.routes");
 const invoiceRoutes = require("./invoice.routes");
-const categoryRoutes = require("./category.routes");
 const monthRoutes = require("../routes/month.routes");
 
 const router = Router();
 
 router.use("/user", userRoutes);
 router.use("/invoice", invoiceRoutes);
-router.use("/category", categoryRoutes);
 router.use("/month", monthRoutes);
 
 module.exports = router;
