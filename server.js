323c7073754a35f6e6f5df4af6db04b449d3dc42
Santiago Estevez <elbolaestevez@gmail.com>
2023-08-24 16:45:07 -0300

    changing port

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const models = require("./models/index");
 const router = require("./routes");
 const cors = require("cors");
+const port = process.env.PORT || 3000;
 
 require("dotenv").config();
 
@@ -13,7 +14,7 @@ app.use(cors(["http://localhost:3000"]));
 app.use("/api", router);
 
 db.sync({ force: false }).then(() => {
-  app.listen(3000, () => {
+  app.listen(port, () => {
     console.log("server levantado en puerto 3000");
   });
 });
